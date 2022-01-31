42f7fe0623633423a5c8d0b30c1dcb20d0f45961
borysenkoomax@gmail.com <borysenkoomax@gmail.com>
2022-01-31 11:30:52 +0200

    add Services, Delete ShoppingCart Price{}

--- Module2HW2/Catalogue.cs
+++ Module2HW2/Catalogue.cs
@@ -14,32 +14,5 @@ namespace Module2HW2
             new Products() { Name = "BMW", Id = 2, Price = 15 },
             new Products() { Name = "KIA", Id = 3, Price = 7 },
         };
-
-        public void ShowProductsInConsole()
-        {
-            Console.WriteLine("Our Products:");
-            foreach (Products i in OurCatalogue)
-            {
-                Console.WriteLine($"Id: {i.Id} Name: {i.Name} Price: {i.Price}");
-            }
-        }
-
-        public Products[] GetAllProducts()
-        {
-            return OurCatalogue;
-        }
-
-        public Products GetOneProduct(int id)
-        {
-            foreach (Products product in GetAllProducts())
-            {
-                if (product.Id == id)
-                {
-                    return product;
-                }
-            }
-
-            return new Products();
-        }
     }
 }
--- Module2HW2/ShoppingCart.cs
+++ Module2HW2/ShoppingCart.cs
@@ -14,47 +14,6 @@ namespace Module2HW2
 
         public decimal OrderPrice { get; set; }
 
-        private decimal Price { get; set; }
-
-        private int Counter { get; set; }
-
-        public void AddOneProductToTheShoppingCart(Products product)
-        {
-            Array.Resize(ref ourShoppingCart, Counter + 1);
-            ourShoppingCart[Counter] = product;
-            Counter++;
-        }
-
-        public void EmptyingTheShoppingCart()
-        {
-            ourShoppingCart = new Products[0];
-            OrderPrice = 0;
-        }
-
-        public void Summ()
-        {
-            foreach (Products product in ourShoppingCart)
-            {
-
-                OrderPrice += product.Price;
-            }
-        }
-
-        public void ShowShoppingCart()
-        {
-            foreach (Products product in ourShoppingCart)
-            {
-                Console.WriteLine($"{product.Id} {product.Name} {product.Price}");
-            }
-        }
-
-        public void GenerateOrder()
-        {
-            Summ();
-            ShoppingCartId = ++ShoppingCartId;
-            Console.WriteLine($"\nYour order number is {ShoppingCartId}\nOrder price {OrderPrice}");
-            ShowShoppingCart();
-            EmptyingTheShoppingCart();
-        }
+        public int Counter { get; set; }
     }
 }
